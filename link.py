97e4aea3fa422f85b6f0f7ce07922e3261bf01f4
Phan Thế Minh Châu <73175450+PhanTheMinhChau@users.noreply.github.com>
2021-12-03 17:39:37 +0700

    Update link.py

--- link.py
+++ link.py
@@ -6,15 +6,18 @@ options= webdriver.ChromeOptions()
 options.headless = True
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 f = open("url.txt", "a")
-for i in range(99):
-    driver.get("https://shopee.vn/Th%E1%BB%9Di-Trang-Nam-cat.11035567?page="+str(i))
-    driver.execute_script ("document.body.style.zoom = '10% '")
-    sleep(2)
-    url = driver.find_elements_by_xpath("//div[@class='col-xs-2-4 shopee-search-item-result__item']/a")
 
-    for a in url:
-        it = a.get_attribute('href')
-        f.writelines(it+"\n")
-        print(it)
-    print(len(url)) 
+url = ["https://shopee.vn/Th%E1%BB%9Di-Trang-Nam-cat.11035567?page=","https://shopee.vn/Th%E1%BB%9Di-Trang-N%E1%BB%AF-cat.11035639?page="]
+for j in url:
+    for i in range(99):
+        driver.get(j+str(i))
+        driver.execute_script ("document.body.style.zoom = '10% '")
+        sleep(2)
+        url = driver.find_elements_by_xpath("//div[@class='col-xs-2-4 shopee-search-item-result__item']/a")
+    
+        for a in url:
+            it = a.get_attribute('href')
+            f.writelines(it+"\n")
+            print(it)
+        print(len(url)) 
 f.close()
